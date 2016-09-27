136eafa46ff1e51e90de615826b68a834fcdd8e0
Alina <alinaxweber@gmail.com>
2016-09-27 21:05:03 +0200

    fixed fatal bug in returnRadomMatches()

--- src/facematch/FaceMatch.java
+++ src/facematch/FaceMatch.java
@@ -1,10 +1,20 @@
 package facematch;
 
+import model.Match;
+
 public class FaceMatch {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(returnRandomMatches());
+	}
+	
+	private static Match[] returnRandomMatches(){
+		Match[] matches = new Match[1];
+		
+		matches[0] = new Match();
+		
+		return matches;
+		
 	}
 
 }
